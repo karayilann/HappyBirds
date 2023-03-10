1c8b8d69a849cf579e8866e794538ee541b2ee3d
karayilann <muhammethamitkarayilan@gmail.com>
2023-03-10 15:52:06 +0300

    Menu design completed without visual elements

--- Assets/Scripts/menuCodes.cs
+++ Assets/Scripts/menuCodes.cs
@@ -3,14 +3,46 @@ using UnityEngine.SceneManagement;
 
 public class menuCodes : MonoBehaviour
 {
-    public GameObject SettingsPanel;
-    public GameObject GeneralPanel;
+    public GameObject SettingsPanel;            // settings screen
+    public GameObject GeneralPanel;             // menu screen panel
+    public GameObject CreditsPanel;             // Credits screen
+    
     public void PlayButton()
     {
         GeneralPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
         SceneManager.LoadScene("Level1");
     }
-
     
+    public void SettingsButton()
+    {
+        GeneralPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+    
+    public void CreditsButton()
+    {
+        GeneralPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+    
+    public void BackButton()
+    {
+        GeneralPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+    }
 
+    public void LikeButton()
+    {
+        Application.OpenURL("https://linktr.ee/muhammetkarayilan");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
